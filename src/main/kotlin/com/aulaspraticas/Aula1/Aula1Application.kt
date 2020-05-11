58411da7ea20fe55846754d93cfc95a14d5530e8
Dyllan Beccari <dyllan.beccari@gmail.com>
2020-05-11 16:21:02 -0600

    Mudancas para Kotlin

---
+++ src/main/kotlin/com/aulaspraticas/Aula1/Aula1Application.kt
@@ -0,0 +1,11 @@
+package com.aulaspraticas.Aula1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Aula1Application
+
+fun main(args: Array<String>) {
+	runApplication<Aula1Application>(*args)
+}
